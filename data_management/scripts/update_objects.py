06408ab73e6af49568bca8dc1e62200397532838
Israel Bismuth <bismuthisrael@gmail.com>
2021-04-29 16:07:34 +0000

    date_enrichment

--- data_management/scripts/update_objects.py
+++ data_management/scripts/update_objects.py
@@ -1,4 +1,7 @@
 from tqdm.contrib.concurrent import process_map
+from tqdm import tqdm
+# import sys
+# sys.path.insert(0, '/home/israel/movie-reco')
 from data_management.mongo_utils import get_collection
 from data_management.movie import Movie
 
@@ -17,8 +20,15 @@ def add_best_similar_data():
     process_map(_add_best_similar_data, ids, max_workers=10, chunksize=100)
 
 
+def add_imdb_data(fields):
+    collection = get_collection()
+    imdb_ids = collection.find({'imdbID': {'$ne': None}}).distinct('imdbID')
+    print(len(imdb_ids), " movies to update")
+    for i, imdbID in tqdm(enumerate(imdb_ids[::-1]), total=len(imdb_ids)):
+        m = Movie(imdbID)
+        m.enrich_from_imdb(fields)
+
+
 if __name__ == "__main__":
-    add_best_similar_data()
-
-
+    add_imdb_data(['year'])
 
